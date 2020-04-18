7a68c166e11ed36921498047bfffdb39c9eee47a
arhamchordia <43543921+arhamchordia@users.noreply.github.com>
2020-04-18 19:27:16 +0530

    Create 779C.cpp

---
+++ 779C.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+struct Seller
+{
+   ll first,second,diff;
+} a[200005];
+
+bool cmp(Seller p, Seller q)
+{
+    return ( p.diff>q.diff);
+}
+int main()
+{
+    ll n,m,i,j,ans=0;
+    cin>>n>>m;
+    for(i=0; i<n; i++){
+        cin>>a[i].first;
+    } 
+    for(i=0; i<n; i++){
+        cin>>a[i].second;
+    }
+    for(i=0; i<n; i++){
+        a[i].diff=(a[i].second-a[i].first );
+    }
+    sort(a,a+n,cmp);
+    for(i=0; i<m; i++){
+        ans+=a[i].first;
+    } 
+    for(i=m; i<n; i++){ 
+        ans+=min(a[i].first, a[i].second);
+    }
+    cout<<ans<<endl;
+}
